843652103895dee100721cf5f8c847e91ffea2db
Vinh Vu <vinhvu5112000@gmail.com>
2021-10-25 11:09:00 +0700

    1

--- public/assets/js/custom.js
+++
@@ -1,7 +0,0 @@
-var hash = location.hash.replace(/^#/, ''); 
-if (hash) {
-    $('.nav-tabs a[href="#' + hash + '"]').tab('show');
-} 
-$('.nav-tabs a').on('shown.bs.tab', function (e) {
-    window.location.hash = e.target.hash;
-})
