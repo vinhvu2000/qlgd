6c667144292660594f63847a45cab40f4afe790e
Vinh Vu <vinhvu5112000@gmail.com>
2021-10-25 10:44:00 +0700

    1

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Controller\Authentication;
+use App\Http\Controllers\Authentication;
 
 /*
 |--------------------------------------------------------------------------
