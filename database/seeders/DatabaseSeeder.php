5cc36ec3a4035a72859f0c26b16c930cffae9884
Vinh Vu <vinhvu5112000@gmail.com>
2021-11-13 22:50:56 +0700

    1

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +17,23 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        DB::table('users')->insert([
+            'name' => "Quản trị hệ thống",
+            'email' => "superadmin@hnue.edu.vn",
+            'password' => Hash::make('12345678'),
+            'role' => "supderadmin"
+        ]);
+        DB::table('users')->insert([
+            'name' => "Quản lí nhà C",
+            'email' => "adminC@hnue.edu.vn",
+            'password' => Hash::make('12345678'),
+            'role' => "admin"
+        ]);
+        DB::table('users')->insert([
+            'name' => "K68D CNTT",
+            'email' => "K68DCNTT@hnue.edu.vn",
+            'password' => Hash::make('12345678'),
+            'role' => "user"
+        ]);
     }
 }
